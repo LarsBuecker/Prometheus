21468da4e8c67a0293b549930dc524a6b14b6ee5
LarsBuecker <48998231+LarsBuecker@users.noreply.github.com>
2020-10-08 14:58:07 +0200

    Moved OpenGL shader implementation to OpenGLShader class

--- Prometheus/src/Prometheus/Renderer/Renderer.cpp
+++ Prometheus/src/Prometheus/Renderer/Renderer.cpp
@@ -1,14 +1,15 @@
 #include "pmpch.h"
 #include "Renderer.h"
 
+#include "Prometheus/Platform/OpenGL/OpenGLShader.h"
 
 namespace Prometheus {
 
-	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData();
+	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData();
 
 	void Renderer::BeginScene(OrthograhicCamera& camera)
 	{
-		m_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
+		s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
 	}
 
 	void Renderer::EndScene()
@@ -18,8 +19,8 @@ namespace Prometheus {
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
