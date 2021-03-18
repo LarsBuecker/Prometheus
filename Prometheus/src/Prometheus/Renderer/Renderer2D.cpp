5ecf038401dc7f7a0693600730a9f224f6093e72
LarsBuecker <lars.buecker2@gmail.com>
2021-03-18 16:34:56 +0100

    Added Rotated quad to renderer2D API

--- Prometheus/src/Prometheus/Renderer/Renderer2D.cpp
+++ Prometheus/src/Prometheus/Renderer/Renderer2D.cpp
@@ -126,7 +126,9 @@ namespace Prometheus {
 		s_Data->TextureShader->SetFloat4("u_Color", color);
 		s_Data->WhiteTexture->Bind();
 
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) 
+			* glm::rotate(glm::mat4(1.0f), rotation, { 0.0f, 0.0f, 1.0f })
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		s_Data->TextureShader->SetMat4("u_Transform", transform);
 
 		s_Data->QuadVertexArray->Bind();
@@ -146,7 +148,9 @@ namespace Prometheus {
 		s_Data->TextureShader->SetFloat4("u_Color", tintColor);
 		texture->Bind();
 
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) 
+			* glm::rotate(glm::mat4(1.0f), rotation,{ 0.0f, 0.0f, 1.0f })
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		s_Data->TextureShader->SetMat4("u_Transform", transform);
 
 		s_Data->QuadVertexArray->Bind();
